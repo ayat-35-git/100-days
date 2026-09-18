c7fd32f5cc5677be8f2bb26724b7c72c60b94fde
ayat-35-git <ayat.35271@stu.upes.ac.in>
2026-09-18 22:37:57 +0530

    Add files via upload

---
+++ day32q63.c
@@ -0,0 +1,34 @@
+Q63: Merge two arrays.
+
+/*
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+
+*/
+#include <stdio.h>
+int main() {
+int arr1[] = {1, 3, 5, 7};
+int arr2[] = {2, 4, 6, 8, 10};
+int n1 = sizeof(arr1) / sizeof(arr1[0]);
+int n2 = sizeof(arr2) / sizeof(arr2[0]);
+int n3 = n1 + n2;
+int mergedArr[n3];
+for (int i = 0; i < n1; i++) {
+mergedArr[i] = arr1[i];
+}
+for (int i = 0; i < n2; i++) {
+mergedArr[n1 + i] = arr2[i]; // Start inserting after the last element of arr1
+}
+printf("Merged array: ");
+for (int i = 0; i < n3; i++) {
+printf("%d ", mergedArr[i]);
+}
+printf("\n");
+return 0;
+}
---
+++ day32q64.c
@@ -0,0 +1,43 @@
+Q64: Find the digit that occurs the most times in an integer number.
+
+/*
+Sample Test Cases:
+Input 1:
+112233
+Output 1:
+1
+
+Input 2:
+887799
+Output 2:
+7
+
+*/
+#include <stdio.h>
+int main() {
+long long n;
+int freq[10] = {0};
+printf("Enter an integer: ");
+scanf("%lld", &n);
+if (n < 0) {
+n = -n;
+}
+if (n == 0) {
+freq[0]++;
+}
+while (n > 0) {
+int digit = n % 10;
+n /= 10;
+}
+int max_freq = 0;
+int most_frequent_digit = 0;
+for (int i = 0; i < 10; i++) {
+if (freq[i] > max_freq) {
+max_freq = freq[i];
+most_frequent_digit = i;
+}
+}
+printf("The digit that occurs the most is %d (appeared %d times).\n", 
+most_frequent_digit, max_freq);
+return 0;
+}
